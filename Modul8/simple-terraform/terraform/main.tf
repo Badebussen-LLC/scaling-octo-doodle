8d9d67bffcd0c4fd3e89aadee272d6adc24d8c5e
Badebussen <alexthuestad@googlemail.com>
2025-11-11 15:30:19 +0100

    Initial push of modul 8 files

---
+++ Modul8/simple-terraform/terraform/main.tf
@@ -0,0 +1,35 @@
+# Random suffix for unique naming
+resource "random_string" "suffix" {
+  length  = 5
+  special = false
+  upper   = false
+}
+
+# Resource Group
+resource "azurerm_resource_group" "main" {
+  name     = "rg-${var.project_name}-${var.environment}"
+  location = var.location
+
+  tags = local.tags
+}
+
+# Storage Account
+resource "azurerm_storage_account" "main" {
+  name                = "st${var.project_name}${var.environment}${random_string.suffix.result}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  min_tls_version = "TLS1_2"
+
+  tags = local.tags
+}
+
+# Storage Container
+resource "azurerm_storage_container" "demo" {
+  name                  = "alth-${var.sa_name}"
+  storage_account_id    = azurerm_storage_account.main.id
+  container_access_type = "private"
+}
