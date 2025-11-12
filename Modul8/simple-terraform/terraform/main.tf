2fe7c3089ece400bbb46abae0ecf7d802ece3867
badebussen <alexthuestad@googlemail.com>
2025-11-12 12:01:32 +0100

    Update main.tf

--- Modul8/simple-terraform/terraform/main.tf
+++ Modul8/simple-terraform/terraform/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_storage_account" "main" {
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
 
-  account_tier             = "Premium"
+  account_tier             = "Standard"
   account_replication_type = "LRS"
 
   min_tls_version = "TLS1_2"
