71aa2f9ecc89ebc5e601b48e08662dfd689926b6
Badebussen <alexthuestad@googlemail.com>
2025-11-12 12:23:47 +0100

    trying again with new files

---
+++ modul8/simple-terraform/shared/backend.hcl
@@ -0,0 +1,6 @@
+resource_group_name  = "rg-tfbackend-alext"
+storage_account_name = "sttfbackendalext"
+container_name       = "tfstate"
+use_azuread_auth     = true
+use_cli              = true
+key                  = "modul8/default/terraform.tfstate"
---
+++ modul8/simple-terraform/terraform/main.tf
@@ -0,0 +1,45 @@
+# Random suffix for unique naming
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
+# Resource Group
+resource "azurerm_resource_group" "main" {
+  name     = "rg-${var.project_name}-${var.environment}"
+  location = var.location
+
+  tags = {
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+    Project     = var.project_name
+    Test        = "CI test"
+    Test2       = "CI 2 test"
+    owner       = "alexander.thuestad@tisipfagskole.no"
+  }
+}
+
+# Storage Account
+resource "azurerm_storage_account" "main" {
+  name                = "st${var.project_name}${var.environment}${random_string.suffix.result}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  min_tls_version = "TLS1_2"
+
+  tags = {
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+}
+
+# Storage Container
+resource "azurerm_storage_container" "demo" {
+  name                  = "demo-data"
+  storage_account_id    = azurerm_storage_account.main.id
+  container_access_type = "private"
+}
