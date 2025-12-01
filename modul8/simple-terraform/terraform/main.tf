fe7463d0e7809d64956140a3e1f2eea761d1d88a
Badebussen <alexthuestad@googlemail.com>
2025-12-01 20:26:27 +0100

    Updated main.tf

--- modul8/simple-terraform/terraform/main.tf
+++ modul8/simple-terraform/terraform/main.tf
@@ -7,7 +7,7 @@ resource "random_string" "suffix" {
 
 # Resource Group
 resource "azurerm_resource_group" "main" {
-  name     = "rg-${var.project_name}-${var.environment}-alext"
+  name     = "rg-${var.project_name}-${var.environment}"
   location = var.location
 
   tags = {
