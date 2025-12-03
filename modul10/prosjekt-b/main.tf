237b27ed6b5f462a24f137de1d4bd94e4c3e5bea
Badebussen <alexthuestad@googlemail.com>
2025-12-03 22:28:17 +0100

    updates to main.tf again

--- modul10/prosjekt-b/main.tf
+++ modul10/prosjekt-b/main.tf
@@ -1,3 +1,29 @@
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+
+  # Bruk environment variables eller federated credentials
+  use_oidc = true
+}
+
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+
 # Mottaker-gruppen
 resource "azurerm_resource_group" "rg_projectb" {
   name     = "rg-mvprojectb-althu"
